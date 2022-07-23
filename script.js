d37af59419fe0d716d88c4fdd9c229e412795d83
Dristanta Silwal <you@example.com>
2022-07-23 17:51:38 +0545

    20 bug changed

--- script.js
+++ script.js
@@ -6,21 +6,22 @@ let actualNum = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.check').addEventListener('click', function () {
     const guess = document.querySelector('.guess').value;
     // document.write(`Value: ${guess}`)
-
-    if (!guess)
-        document.querySelector('.message').textContent = 'Enter a number';
-    else if (guess < 1 || guess > 20)
+    // if (guess > 20)
+    //     document.querySelector('.message').textContent = 'Choose below 20';
+    if (guess > 20 || guess < 0)
         document.querySelector('.message').textContent = 'The number should be between 1 and 20';
+    else if (!guess)
+        document.querySelector('.message').textContent = 'Enter a number';
     else if (guess == actualNum) {
         document.querySelector('.number').textContent = actualNum;
         document.querySelector('.message').textContent = 'Correct number';
         document.querySelector('body').style.backgroundColor = 'green';
 
-        if(score > highscore){
+        if (score > highscore) {
             highscore = score;
-             document.querySelector('.highscore').textContent = highscore;
+            document.querySelector('.highscore').textContent = highscore;
         }
-       
+
     }
     else if (guess > actualNum) {
         document.querySelector('.message').textContent = 'Go a little lower';
@@ -35,7 +36,7 @@ document.querySelector('.check').addEventListener('click', function () {
 
 });
 
-document.querySelector('.again').addEventListener('click', function() {
+document.querySelector('.again').addEventListener('click', function () {
     actualNum = Math.trunc(Math.random() * 20) + 1;
     score = 20;
     document.querySelector('.score').textContent = 20;
