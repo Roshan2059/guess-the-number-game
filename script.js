0d022b90326c57212ee651df5096d62e4910078a
Dristanta Silwal <dristantasilwal003@gmail.com>
2023-06-16 22:23:20 -0400

    Edit: Script.js
    Changed code into functions

--- script.js
+++ script.js
@@ -1,46 +1,61 @@
 'use strict';
+
 let score = 20;
 let highscore = 0;
 let actualNum = Math.trunc(Math.random() * 20) + 1;
-// document.querySelector('.number').textContent = actualNum;
-document.querySelector('.check').addEventListener('click', function () {
-    const guess = document.querySelector('.guess').value;
-    // document.write(`Value: ${guess}`)
-    // if (guess > 20)
-    //     document.querySelector('.message').textContent = 'Choose below 20';
-    if (guess > 20 || guess < 0)
-        document.querySelector('.message').textContent = 'The number should be between 1 and 20';
-    else if (!guess)
-        document.querySelector('.message').textContent = 'Enter a number';
-    else if (guess == actualNum) {
-        document.querySelector('.number').textContent = actualNum;
-        document.querySelector('.message').textContent = 'Correct number';
+
+const messageElement = document.querySelector('.message');
+const numberElement = document.querySelector('.number');
+const scoreElement = document.querySelector('.score');
+const guessElement = document.querySelector('.guess');
+
+function displayMessage(message) {
+    messageElement.textContent = message;
+}
+
+function displayScore(score) {
+    scoreElement.textContent = score;
+}
+
+function generateRandomNumber() {
+    return Math.trunc(Math.random() * 20) + 1;
+}
+
+function resetGame() {
+    actualNum = generateRandomNumber();
+    score = 20;
+    displayScore(score);
+    guessElement.value = '';
+    numberElement.textContent = '?';
+    document.querySelector('body').style.backgroundColor = '#222';
+}
+
+function checkGuess() {
+    const guess = Number(guessElement.value);
+
+    if (guess > 20 || guess < 1) {
+        displayMessage('The number should be between 1 and 20');
+    } else if (!guess) {
+        displayMessage('Enter a number');
+    } else if (guess === actualNum) {
+        numberElement.textContent = actualNum;
+        displayMessage('Correct number');
         document.querySelector('body').style.backgroundColor = 'green';
 
         if (score > highscore) {
             highscore = score;
             document.querySelector('.highscore').textContent = highscore;
         }
-
-    }
-    else if (guess > actualNum) {
-        document.querySelector('.message').textContent = 'Go a little lower';
+    } else if (guess > actualNum) {
+        displayMessage('Go a little lower');
         score--;
-        document.querySelector('.score').textContent = score;
-    }
-    else if (guess < actualNum) {
-        document.querySelector('.message').textContent = 'Go a little higher';
+        displayScore(score);
+    } else if (guess < actualNum) {
+        displayMessage('Go a little higher');
         score--;
-        document.querySelector('.score').textContent = score;
+        displayScore(score);
     }
+}
 
-});
-
-document.querySelector('.again').addEventListener('click', function () {
-    actualNum = Math.trunc(Math.random() * 20) + 1;
-    score = 20;
-    document.querySelector('.score').textContent = 20;
-    document.querySelector('.guess').value = '';
-    document.querySelector('.number').textContent = '?';
-    document.querySelector('body').style.backgroundColor = '#222';
-}); 
+document.querySelector('.check').addEventListener('click', checkGuess);
+document.querySelector('.again').addEventListener('click', resetGame);
